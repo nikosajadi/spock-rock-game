02015ec6e1d84f05bf15362ab3cd71a65891be46
nikosajadi <na.sajadii.29@gmail.com>
2023-02-27 23:58:27 +0100

    if else : playerChoice is eqial with computerChoice

--- script.js
+++ script.js
@@ -92,6 +92,9 @@ function updateScore(playerChoice){
 console.log(playerChoice,computerChoice);
 if (playerChoice === computerChoice) {
   resultText.textContent ="It's a tie!";
+} else {
+  const choice = choices [playerChoice];
+  console.log(choice.defeats.indexOf(computerChoice));
 }
 }
 
